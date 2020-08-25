746b650e18330ce8cc47d23840a308e3430b1950
Aleksejs Muzicenko <a.muzicenko@eco-fin.eu>
2020-08-25 17:00:10 +0300

    Translations added

---
+++ src/i18n/ru/index.js
@@ -0,0 +1,7 @@
+export default {
+  language: "Русский",
+  settings: "Настройки",
+  languages: "Языки",
+  quit: "Выйти",
+  clearAllData: "Очистить все данные"
+};
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,6 +11,46 @@
         <q-btn dense flat icon="crop_square" @click="maximize" />
         <q-btn dense flat icon="close" @click="closeApp" />
       </q-bar>
+      <div class="q-pa-sm q-pl-md row items-center">
+        <div class="cursor-pointer non-selectable">
+          {{ $t("settings") }}
+          <q-menu>
+            <q-list dense style="min-width: 100px">
+              <q-item clickable>
+                <q-item-section>{{ $t("languages") }}</q-item-section>
+                <q-item-section side>
+                  <q-icon name="keyboard_arrow_right" />
+                </q-item-section>
+
+                <q-menu anchor="top right" self="top left">
+                  <q-list>
+                    <q-item
+                      v-for="(value, key) in langOptions"
+                      :key="key"
+                      dense
+                      clickable
+                      v-close-popup
+                    >
+                      <q-item-section @click="changeLanguage(value.lang)">{{
+                        value.label
+                      }}</q-item-section>
+                    </q-item>
+                  </q-list>
+                </q-menu>
+              </q-item>
+              <q-item clickable v-close-popup @click="clearData">{{
+                $t("clearAllData")
+              }}</q-item>
+
+              <q-separator />
+
+              <q-item clickable v-close-popup>
+                <q-item-section>{{ $t("quit") }}</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </div>
+      </div>
     </q-header>
 
     <q-page-container>
@@ -22,6 +62,24 @@
 <script>
 export default {
   name: "MainLayout",
+  created() {
+    this.$i18n.locale = this.$electronStore.get("locale") || "en-us";
+  },
+  data() {
+    return {
+      lang: this.$electronStore.get("locale"),
+      langOptions: [
+        { lang: "en-us", label: "English" },
+        { lang: "ru", label: "Russian" }
+      ]
+    };
+  },
+  watch: {
+    lang(language) {
+      this.$i18n.locale = language;
+      this.$electronStore.set("locale", language);
+    }
+  },
   computed: {
     isElectron() {
       return process.env.MODE === "electron";
@@ -48,6 +106,13 @@ export default {
       if (this.isElectron) {
         this.$q.electron.remote.BrowserWindow.getFocusedWindow().close();
       }
+    },
+    changeLanguage(language) {
+      this.lang = language;
+    },
+    clearData() {
+      this.$electronStore.clear();
+      this.$q.electron.remote.BrowserWindow.getFocusedWindow().reload();
     }
   }
 };
